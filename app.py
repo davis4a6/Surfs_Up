b8f215f0276c61628bc53599d60c77b30496bb05
Ariana Davis <arianadavisj@gmail.com>
2023-01-20 16:04:32 -0500

    adding app.py

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def i_love_nick():
-    return 'I love Nick'
+def hello_world():
+    return 'Hello World'
+
+@app.route('/home')
+def index():
+    return 'Hello World! Welcome to my Home Page!'
+
+@app.route('/about')
+def about():
+    name = "Ariana"
+    location = "Akron/Warren/Youngstown"
+    return f"My name is {name}, and I provide services in the {location} area."
+
+@app.route('/contact')
+def contact():
+    email = "adavis@gmail.com"
+    return f"Questions? Please reach out to {email}."
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
